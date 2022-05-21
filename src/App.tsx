aa5b1b375af6f244aa58c43640e7ac88107e33ff
gigasharkio <101996619+gigasharkio@users.noreply.github.com>
2022-05-21 01:06:38 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import "./styles.css";
+import { useOnboard } from "use-onboard";
+
+export default function App() {
+  const {
+    selectWallet,
+    address,
+    isWalletSelected,
+    disconnectWallet,
+    balance
+  } = useOnboard();
+  return (
+    <div>
+      {
+        <button
+          onClick={async () => {
+            if (isWalletSelected) disconnectWallet();
+            else await selectWallet();
+          }}
+        >
+          {isWalletSelected ? "Disconnect" : "Connect"}
+        </button>
+      }
+      <p>Address: {address}</p>
+      <p>Balance: {balance} ETH</p>
+    </div>
+  );
+}
